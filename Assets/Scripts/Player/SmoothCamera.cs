3fac1333583cc1de1ceb7436948c65b18a330a62
BenWillemsma <ben.willemsma@gmail.com>
2019-01-26 11:26:53 -0500

    Base Spline Movement

---
+++ Assets/Scripts/Player/SmoothCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        if (target)
+        {
+            transform.position = target.position + Quaternion.FromToRotation(Vector3.forward, target.right) * offset;
+            transform.LookAt(target);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            transform.position = Vector3.Lerp
+                (
+                    transform.position,
+                    target.position + Quaternion.FromToRotation(Vector3.forward, target.right) * offset,
+                    Time.deltaTime * 8
+                );
+            transform.rotation = Quaternion.Lerp
+                (
+                    transform.rotation,
+                    Quaternion.LookRotation(target.position - transform.position, Vector3.up),
+                    Time.deltaTime * 3
+                );
+        }
+    }
+}
