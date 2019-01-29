425ff980dc221f602dfdb638cf6a632c3ba896ab
Sydney Kim-Buu Lu <sydneykimbuulu@gmail.com>
2019-01-28 22:05:05 -0500

    Script for Audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource MenuAudio;
+
+    void Start()
+    {
+        MenuAudio.Play(0);
+        Debug.Log("Music Start");
+    }
+
+    void Update()
+    {
+
+    }
+}
